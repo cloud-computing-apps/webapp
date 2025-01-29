283c4a98db9ff88de58bd3965ef58fcbc52d47cc
Nidhi Kulkarni <kulkarni.nidh@northeastern.edu>
2025-01-28 22:40:54 -0500

    code cleanup + header

--- api/handlers/health.go
+++ api/handlers/health.go
@@ -9,6 +9,10 @@ import (
 
 func HealthCheckHandler(dbConnection *gorm.DB) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Cache-Control", "no-cache, no-store, must-revalidate")
+		w.Header().Set("Content-Type", "application/json")
+		w.Header().Set("X-Content-Type-Options", "nosniff")
+
 		if r.Method != http.MethodGet {
 			w.WriteHeader(http.StatusMethodNotAllowed)
 			return
@@ -23,10 +27,9 @@ func HealthCheckHandler(dbConnection *gorm.DB) http.HandlerFunc {
 			w.WriteHeader(http.StatusBadRequest)
 			return
 		}
-		w.Header().Set("Cache-Control", "no-cache, no-store, must-revalidate")
 
 		healthCheck := db.HealthCounter{
-			Datetime: time.Now(),
+			Datetime: time.Now().UTC(),
 		}
 
 		if err := dbConnection.Create(&healthCheck).Error; err != nil {
