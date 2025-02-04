96e00cd05a34c4f1639b72017eb746cf875bf990
Nidhi Kulkarni <kulkarni.nidh@northeastern.edu>
2025-02-03 22:17:04 -0500

    gorm interface to support tests

--- db/models.go
+++ db/models.go
@@ -1,10 +1,15 @@
 package db
 
 import (
+	"gorm.io/gorm"
 	"time"
 )
 
 type HealthCounter struct {
 	CheckId  uint      `gorm:"primaryKey;autoIncrement"`
 	Datetime time.Time `gorm:"not null"`
+}
+
+type Database interface {
+	Create(value interface{}) *gorm.DB
 }
