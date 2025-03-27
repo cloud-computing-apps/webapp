463094c715e1ee75bc282753e4d5cf6b625682dd
Nidhi Kulkarni <kulkarni.nidh@northeastern.edu>
2025-03-26 20:48:38 -0400

    syslog and app log group split

--- packer/scripts/install_cloudwatch.sh
+++ packer/scripts/install_cloudwatch.sh
@@ -6,3 +6,6 @@ sudo dpkg -i amazon-cloudwatch-agent.deb
 sudo apt-get install -f
   
 sudo mv /tmp/config.json /opt/aws/amazon-cloudwatch-agent/bin/config.json
+
+sudo touch /var/log/webapp.log
+sudo chown csye6225:csye6225 /var/log/webapp.log
